42b9a9277e682b6c64ad45d87b1545228e8e58f6
JuanRojas2010 <juanpisrojascortes2010@gmail.com>
2026-07-16 08:37:26 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ btnReportar.addEventListener('click', () => {
 btnManual.addEventListener('click', () => {
     alert("Abriendo el manual de usuario");
 });
+// 1. Seleccionamos el botón y el menú nav
+const botonMenu = document.querySelector('#boton-menu');
+const menuNavegacion = document.querySelector('#navegacion');
+
+// 2. Escuchamos el clic en el botón hamburguesa
+botonMenu.addEventListener('click', () => {
+    // 3. El toggle quita o pone la clase "menu-oculto" dinámicamente
+    menuNavegacion.classList.toggle('menu-oculto');
+});
